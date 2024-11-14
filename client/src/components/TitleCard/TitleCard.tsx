84fefdbff7443e2124b13ac89e865e1b1bf44fe8
LucasBbe <lucas.boube@gmail.com>
2024-11-14 10:05:44 +0100

    🚧 Réalisation de la structure de la carte title

---
+++ client/src/components/TitleCard/TitleCard.tsx
@@ -0,0 +1,14 @@
+import "./TitleCard.css";
+
+function TitleCard() {
+  return (
+    <>
+      <section className="titleCard">
+        <img src="images/ReferenceArtiste.jpg" alt="Potrait de l'artiste" />
+        <span>Titre de la chansson</span>
+      </section>
+    </>
+  );
+}
+
+export default TitleCard;
