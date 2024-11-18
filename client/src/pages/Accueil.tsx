75a49c078c3d4f801131c63665ebb404c5711719
DamienD44 <damas44@gmail.com>
2024-11-18 16:10:28 +0100

    waiting denifinitly dataBase for end of work

--- client/src/pages/Accueil.tsx
+++ client/src/pages/Accueil.tsx
@@ -7,22 +7,4 @@ function Accueil() {
       <Carousel />
     </>
   );
-import musicData from "../assets/music-data.json";
-import MusicSections from "../components/MusicSections/MusicSections";
 
-function App() {
-  return (
-    <main>
-      {musicData.map((el) => {
-        return (
-          <MusicSections
-            key={`${el.genre}-${el.Artistes[0].id}`} // Utiliser genre + id comme clé unique
-            Artists={el.Artistes}
-          />
-        );
-      })}
-    </main>
-  );
-}
-
-export default App;
