dc3de438c217c36a103f6c7c205767d5fbcdda78
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2024-11-19 16:08:58 +0100

    The DB is available on the server.

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,3 +1,4 @@
+import * as fs from "node:fs";
 import express from "express";
 
 const router = express.Router();
@@ -12,6 +13,13 @@ import itemActions from "./modules/item/itemActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+router.get("/api/music-data", (req, res) => {
+  const rawdata = fs.readFileSync("./src/assets/music-data.json");
+  const dataMusic = JSON.parse(rawdata.toString());
+  console.warn(dataMusic);
+  res.json(dataMusic);
+});
 
 /* ************************************************************************* */
 
