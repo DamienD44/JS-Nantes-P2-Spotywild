6f85007bb08eee31c93ed92e56caa965dbc6623f
DamienD44 <damas44@gmail.com>
2024-11-21 14:15:16 +0100

    resolve pull conflit

--- client/src/pages/Albums.tsx
+++ client/src/pages/Albums.tsx
@@ -1,5 +1,74 @@
+import "./Album.css";
+import { MusicData } from "../types/musicSection";
+const album = [
+  {
+    id: 1,
+    name: "David Bowie",
+    country: "United Kingdom",
+    imgSrc: "https://img-lumas.b-cdn.net/showimg_gve35_search.jpg",
+    description: "Iconic musician known for his glam rock style.",
+  },
+  {
+    id: 1,
+    albumName: "Let's Dance",
+    albumImg:
+      "https://static.fnac-static.com/multimedia/FR/Images_Produits/FR/fnac.com/Visual_Principal_340/1/0/6/0724352189601/tsp20120925110227/Let-s-dance.jpg",
+    description:
+      "Let's Dance est un album emblématique de David Bowie, sorti en 1983. C'est le quinzième album de Bowie et marque un tournant dans sa carrière, avec une approche plus orientée vers la pop et le funk rock. Produit par Nile Rodgers, le son de l'album est plus accessible et dansant, intégrant des éléments de musique funk, soul et new wave. ",
+    songs: [
+      "Modern Love",
+      "China Girl",
+      "Let's Dance",
+      "Without You",
+      "Ricochet",
+      "Criminal World",
+      "Cat People",
+      "Shake It",
+    ],
+  },
+  {
+    id: 2,
+    albumName: "The Man Who Sold the World",
+    albumImg:
+      "https://static.fnac-static.com/multimedia/Images/FR/NR/c4/a6/76/7775940/1507-1/tsp20160208120357/Man-who-sold-the-world.jpg",
+    description:
+      "The Man Who Sold the World est le troisième album studio de David Bowie, sorti en 1970. Cet album marque une étape importante dans l'évolution musicale de Bowie, avec un son plus lourd et expérimental, qui mélange rock psychédélique, hard rock et des influences progressives. Il est souvent perçu comme un album précurseur de ce qui deviendra le glam rock. Le titre phare de l'album, The Man Who Sold the World, est un morceau mystique et introspectif, qui aborde des thèmes de déshumanisation et d'aliénation.",
+    songs: [
+      "The Width of a Circle",
+      "All the Madmen",
+      "Black Country Rock",
+      "After All",
+      "Running Gun Blues",
+      "Saviour Machine",
+      "She Shook Me Cold",
+      "The Man Who Sold the World",
+      "The Supermen",
+    ],
+  },
+  {
+    id: 3,
+    albumName: "Heroes",
+    albumImg:
+      "https://i.scdn.co/image/ab67616d0000b27335e493b3a60aa4bcaa91344f",
+    description:
+      "Heroes est le douzième album studio de David Bowie, sorti en 1977. Il fait partie de la trilogie berlinoise, réalisée en collaboration avec Brian Eno, qui comprend également Low et Lodger. Cet album est un mélange innovant de rock, d'électronique et de musique expérimentale, avec une forte influence de l'ambient et du krautrock, un genre musical venu d'Allemagne.",
+    songs: [
+      "Beauty and the Beast",
+      "Joe the Lion",
+      "Heroes",
+      "Sons of the Silent Age",
+      "Blackout",
+      "V-2 Schneider",
+      "Sense of Doubt",
+      "Moss Garden",
+      "Neuköln",
+      "The Secret Life of Arabia",
+    ],
+  },
+];
 function Albums() {
-  return <h1>Page Albums</h1>;
+  /*return <h1>Page Albums</h1>;*/
+  return;
 }
 
 export default Albums;
