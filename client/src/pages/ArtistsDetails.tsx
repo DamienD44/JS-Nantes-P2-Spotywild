80623acc9191391da0043902e70e3431e521aaad
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2024-11-25 09:58:10 +0100

    Feature 39 is done.

--- client/src/pages/ArtistsDetails.tsx
+++ client/src/pages/ArtistsDetails.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 
 export default function ArtistDetails() {
   const [artDescription, setArtDescription] = useState<string>("");
-
+  console.info(DOMPurify);
   const dataMusic = useLoaderData() as ArtistI;
 
   const albums = dataMusic.albums;
